aa342a9125ae3a037158957a735f887d9e52bb61
Anderson Matthew J <andersonmatthewj@johndeere.com>
2021-07-22 21:42:58 -0500

    completed step 4 - coloring square as single color

--- pixart/pixart.js
+++ pixart/pixart.js
@@ -1,19 +1,27 @@
+// identifying and saving document elements to use
 const button = document.querySelector('#set-color');
 const colorInput = document.querySelector('#color-field');
 const brush = document.querySelector('.brush');
 
+// listens for the button to be clicked, then saves the chosen background color as the brush color
 button.addEventListener('click', function(event) {
     event.preventDefault();
     brush.style.backgroundColor = colorInput.value;
 })
 
 for (let i = 0; i < 20; i++) {
+    // identifying body element
     const body = document.querySelector('body');
+
+    // this will append/create more div elements in body with the class square
     let moreDivs = document.createElement("div");
     moreDivs.setAttribute('class','square');
     body.appendChild(moreDivs);
-}
 
-// console.log(button);
-// console.log(colorText);
-// console.log(brushColor);
+    // this disables the screen refresh with the hint noted in the text, and listens for clicks in any moreDivs
+    moreDivs.addEventListener('click', function(event) {
+        event.preventDefault();
+        // colors any clicked moreDivs green
+        moreDivs.style.backgroundColor = "green";
+        })
+}
